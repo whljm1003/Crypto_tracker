64e70b100e104e6b9fe8348d552d55c6c95b8095
이정민 <leejungmin@MacBook-Pro.local>
2021-12-10 19:52:16 +0900

    done! 4.13 Price Chart part Two

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -75,7 +75,6 @@ const Tab = styled.span<{ isActive: boolean }>`
   font-size: 12px;
   font-weight: 400;
   background-color: rgba(0, 0, 0, 0.5);
-  padding: 7px 0px;
   border-radius: 10px;
   color: ${(props) =>
     props.isActive ? props.theme.accentColor : props.theme.textColor};
